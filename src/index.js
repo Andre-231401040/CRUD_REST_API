7efaa0a00eb792ce7831992d401e1f71bfbc20af
Andre <andrelim865@gmail.com>
2025-07-23 15:16:02 +0700

    establish user routes, error handling middleware, user service model, user controller

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import pool from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import errorHandling from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
@@ -13,8 +15,10 @@ app.use(express.json());
 app.use(cors());
 
 // routes
+app.use("/api", userRoutes);
 
 // error handling middlewares
+app.use(errorHandling);
 
 // testing postgres connection
 app.get("/", async (req, res) => {
