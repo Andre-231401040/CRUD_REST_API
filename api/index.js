1dcac0d9c3a2a6b5a60303eeb97742f0b634f84c
Andre <andrelim865@gmail.com>
2025-07-27 16:06:34 +0700

    change ServerlessHttp to servesless

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import app from "../src/app.js";
 import createUserTable from "../src/data/createUserTable.js";
-import ServerlessHttp from "serverless-http";
+import serverless from "serverless-http";
 
 createUserTable();
 
-export const handler = ServerlessHttp(app);
+export const handler = serverless(app);
